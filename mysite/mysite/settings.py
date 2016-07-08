85a79bf4c51d07ff528a1a0c970f0995911d60f7
gcdeng <ericdenggc@gmail.com>
2016-07-08 16:49:23 +0800

    create and active models

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig', # tell our project that the polls app is installed.
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls',
 ]
 
 MIDDLEWARE_CLASSES = [
